4188d4e473f960ca953ca4180b8b6e69dcede2c5
Davi Ribeiro dos Santos <davi.ribeirosantos11@gmail.com>
2021-08-04 09:41:00 -0300

    Add dependecies

---
+++ app/src/main/java/com/example/application/billsplitingapp/home/HomeActivity.kt
@@ -0,0 +1,15 @@
+package com.example.application.billsplitingapp.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+//import androidx.activity.compose
+
+class HomeActivity: ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
